fdf2d5d4f1ea3cb810726229177916036ad7e0f2
Apelzino <citrus.apelsino@gmail.com>
2023-06-27 19:15:52 +0300

    изменён код

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ from telebot import types
 bot = telebot.TeleBot('6059118014:AAHKFxLsCFxoQ23o4EMiN_NPO_eRi20JTIE')
 @bot.message_handler()
 def start(message):
-    bot.send_message(message.chat.id, 'aye')asdasdфыв
+    bot.send_message(message.chat.id, 'aye')
 
 bot.polling(none_stop=True)
